de8f810159f2370a116be113e8ea369641b7b6a6
Mykola Gurov <ngurov@gmail.com>
2024-08-23 20:19:28 +0200

    ok

--- src/pages/fetching/TwoFetch.tsx
+++ src/pages/fetching/TwoFetch.tsx
@@ -4,7 +4,9 @@ export default function GuidesFetchingData() {
     return (
         <>
             <section class="bg-pink-100 text-gray-700 p-8">
-                <App />
+                <DependentFetch_Nested />
+                <DependentFetch />
+                
             </section>
         </>
     )
@@ -19,8 +21,42 @@ const fetchUser = async (id: string) => {
     return response.json();
 }
 
-function App() {
+function DependentFetch() {
 
+    const [user1] = createResource(() => fetchUser("1"));
+    const [user2] = createResource(user1, (u) => fetchUser(u.mass));
+
+    return (
+        <div>
+            <div>
+                User1:
+                <Switch>
+                    <Match when={user1.loading}>Loading...</Match>
+                    <Match when={user1.error}>
+                        <span data-testid="error">Error: {user1.error.message}</span>
+                    </Match>
+                    <Match when={user1()}>
+                        <div data-testid="result">{JSON.stringify(user1())}</div>
+                    </Match>
+                </Switch>
+            </div>
+            <div>
+                User2:
+                <Switch>
+                    <Match when={user2.loading}>Loading...</Match>
+                    <Match when={user2.error}>
+                        <span data-testid="error">Error: {user2.error.message}</span>
+                    </Match>
+                    <Match when={user2()}>
+                        <div data-testid="result">{JSON.stringify(user2())}</div>
+                    </Match>
+                </Switch>
+            </div>
+        </div>
+    );
+}
+
+function DependentFetch_Nested() {
     const [user1] = createResource(() => fetchUser("1"));
     const [user2] = createResource(user1, (u) => fetchUser(u.mass));
 
