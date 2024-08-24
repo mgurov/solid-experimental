43b46351b3749fe8ca29c5810f4b609de74147a4
Mykola Gurov <ngurov@gmail.com>
2024-08-24 07:50:40 +0200

    good

--- src/pages/context/NestedContexts.tsx
+++ src/pages/context/NestedContexts.tsx
@@ -1,10 +1,31 @@
+import { createContext, JSX } from "solid-js"
 
 export default function GuidesFetchingData() {
     return (
         <>
             <section class="bg-pink-100 text-gray-700 p-8">
-                Hello
+                <TreeProvider name="root">
+                    Here be root
+                </TreeProvider>
             </section>
         </>
+    )
+}
+
+type TreeNode = {
+    name: string,
+    path: string[],
+}
+
+const TreeContext = createContext<TreeNode>()
+
+export function TreeProvider(props: {name: string, path?: string[], children: JSX.Element}) {
+    return (
+        <TreeContext.Provider value={{
+            name: props.name,
+            path: props.path || []
+        }}>
+            {props.children}
+        </TreeContext.Provider>
     )
 }
